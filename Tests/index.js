1dd4b73683439cb805b02132d98931a4aedef552
Christopher <christo_campiglia@hotmail.com>
2018-03-28 19:53:52 -0300

    [ADD] Tests.

---
+++ Tests/index.js
@@ -0,0 +1,14 @@
+const Chain = require ("./.../Chain.js");
+
+const MyChain = new Chain ("kkk");
+
+MyChain.NewBlock ("lorem ipsum");
+MyChain.NewBlock (["lorem ipsum"]);
+MyChain.NewBlock (0);
+MyChain.NewBlock ([0, 1, 2, 3.1]);
+MyChain.NewBlock ({
+	0: "lorem ipsum",
+	1: ["lorem ipsum"],
+	2: 0,
+	3: [0, 1, 2, 3.1]
+});
