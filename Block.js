55ed25eb050583143f175b143fb848a46dd7c76c
Christopher <christo_campiglia@hotmail.com>
2018-03-29 16:40:45 -0300

    [ADD][FIX] Code Docs & Few Code Changes.

--- Block.js
+++ Block.js
@@ -1,41 +1,115 @@
+/**
+ * Get the hash library.
+ * @type {Object}
+ */
 const Hash = require ("hash.js");
 
+/**
+ * Get the encryption library.
+ * @type {Object}
+ */
 const BlowFish = require ("egoroof-blowfish");
 
 
+
+/**
+ * Main Block class, hold the block structure and a few methods.
+ */
 class Block {
 
+	/**
+	 * Instantiate the class, defining: previous hash, genesis block hash,
+	 * the hash of this Block, the content and the timeStamp.
+	 * 
+	 * @param  {String} PreviousHash The Hash used by the last Block.
+	 * @param  {String} Content      The data to be stored.
+	 * @return {Object}              Return the configured Block object.
+	 */
 	constructor (PreviousHash, Content) {
 
+		/**
+		 * The hash used in the last Block.
+		 * @type {String}
+		 */
 		this.PreviousHash = PreviousHash;
 
+		/**
+		 * The hash of this Block.
+		 * @type {String}
+		 */
 		this.CurrentHash = undefined;
 
+		/**
+		 * The data to be stored.
+		 * @type {String}
+		 */
 		this.Content = JSON.stringify (Content);
 
+		/**
+		 * The timestamp of this Block.
+		 * @type {String}
+		 */
 		this.TimeStamp = JSON.stringify (new Date ());
 	}
 
+	/**
+	 * Generate the Hash of this Block based on the Content, PreviousHash and the TimeStamp.
+	 */
 	GenBlockHash () {
 
-		this.EncryptBlockContent ();
+		/**
+		 * Encrypt the conent of the Block.
+		 */
+		this.__EncryptBlockContent__ ();
 
+		/**
+		 * Concaten everything that's gonna be hashed.
+		 * @type {String}
+		 */
 		const ThingsToBeHashed = `${this.Content}${this.PreviousHash}${this.TimeStamp}`;
 
+		/**
+		 * Hash ThingsToBeHashed and set it as the CurrentHash.
+		 * @type {String}
+		 */
 		this.CurrentHash = Hash.sha256 ().update (ThingsToBeHashed).digest ("hex");
 	}
 
-	EncryptBlockContent () {
+	/**
+	 * Encrypt the content of the block to 8 bit Array using the first 8 bits 
+	 * of the PreviousHash.
+	 */
+	__EncryptBlockContent__ () {
 
-		const BF = new BlowFish (this.PreviousHash, 
-			BlowFish.MODE.ECB, BlowFish.PADDING.NULL);
+		/**
+		 * Set the BlowFish with the PreviousHash as key, the ECB mode and the padding to null.
+		 * @type {Object}
+		 */
+		const BF = new BlowFish (this.PreviousHash, BlowFish.MODE.ECB, BlowFish.PADDING.NULL);
 
+		/**
+		 * Set the BlowFish Iv to the first 8 bits in PreviousHash.
+		 */
 		BF.setIv (this.PreviousHash.slice (0, 8));
 
+		/**
+		 * Encode the content of this Block and convert it to a JSON String.
+		 * @type {String}
+		 */
 		const EncodedContent = JSON.stringify (BF.encode (this.Content));
 
+		/**
+		 * Set the Block Content based on the EncodedContent and puting inside an object.
+		 * @type {Object}
+		 */
 		this.Content = JSON.stringify ({Content: EncodedContent});
 	}
 }
 
+
+
+/**
+ * Export the Block class.
+ * @type {Object}
+ */
 module.exports = Block;
--- Chain.js
+++ Chain.js
@@ -1,26 +1,75 @@
+/**
+ * Get the Block class.
+ * @type {Object}
+ */
 const Block = require ("./Block.js");
 
+/**
+ * Get the hash library.
+ * @type {Object}
+ */
 const Hash = require ("hash.js");
 
+/**
+ * Get the file system library.
+ * @type {Object}
+ */
 const FS = require ("fs");
 
+/**
+ * Get the path library.
+ * @type {Object}
+ */
 const Path = require ("path");
 
 
+
+
+/**
+ * Set a constant to be used as Genesis Block Hash to the first block added to each DB.
+ * @type {[type]}
+ */
 const GENESIS_BLOCK_HASH = Hash.sha256 ().update ("chainDB").digest ("hex");
 
 
+
+/**
+ * Main Chain class, build new Blocks and manipulate them.
+ */
 class Chain {
 
+	/**
+	 * Configure an empty Chain and the name of it.
+	 * 
+	 * @param  {String} ChainName The name of the Chain.
+	 * @return {Object} Return the configured Chain object.
+	 */
 	constructor (ChainName = "chainDB") {
 
+		/**
+		 * Store all Blocks of the Chain.
+		 * @type {Array}
+		 */
 		this.Chain = [];
 
+		/**
+		 * The Name of the Chain, used to store the DB in a path with the Chain name.
+		 * @type {String}
+		 */
 		this.ChainName = ChainName;
 	}
 
+	/**
+	 * Open the Chain file.
+	 * 
+	 * @param {String} ChainName The name of the Chain to be opened.
+	 */
 	Open (ChainName) {
 
+		/**
+		 * Read the Chain file by a given name and assign to Chain if 
+		 * it be successfull.
+		 */
 		this.__GetChainFile__ (ChainName);
 	}
 
@@ -30,25 +79,60 @@ class Chain {
 	//////////////////////////////////////////////////////////////
 
 
+	/**
+	 * Create the Chain path if it doesn't exists.
+	 * Create a new Block and encrypt it's content.
+	 * 
+	 * @param {Any} The data to be stored in the Block.
+	 */
 	NewBlock (Content) {
 
+		/**
+		 * Make the Chain folder to store all blocks using the name of this Chain.
+		 */
 		this.__MakeChainPath__ ();
 
+
+
+		/**
+		 * Create e new Block based on the hash of the previous Block and add the 
+		 * content of it.
+		 * @type {Object}
+		 */
 		this.CurrentBlock = new Block (this.__PreviousBlockHash__, Content);
 
+		/**
+		 * Generate the Hash of this Block.
+		 */
 		this.CurrentBlock.GenBlockHash ();
 
+
+		/**
+		 * Add the Block to the Chain.
+		 * @type {}
+		 */
 		this.Chain.push ({
 
 			PreviousBlockHash: this.CurrentBlock.PreviousHash,
 			CurrentBlockHash: this.CurrentBlock.CurrentHash
 		});
 
+		/**
+		 * Save the Block to the Chain.
+		 */
 		this.SaveBlock ();
 	}
 
+	/**
+	 * Save the Block to the Chain path, using the Block hash as the name of the file.
+	 */
 	SaveBlock () {
 
+		/**
+		 * Try create the Block file name inside the path of the Chain with the name
+		 * based on the hash of it.
+		 * If it fails, throw the Error.
+		 */
 		try {
 
 			FS.writeFileSync (`${this.ChainName}/${this.CurrentBlock.CurrentHash}`, 
@@ -59,6 +143,9 @@ class Chain {
 			throw (SomeError);
 		}
 
+		/**
+		 * After save the Block to the Chain at your path, update the Chain file.
+		 */
 		this.__MakeChainFile__ ();
 	}
 
@@ -68,8 +155,18 @@ class Chain {
 	// 			Chains And Blocks Files Manipulators			 //
 	///////////////////////////////////////////////////////////////
 
+
+	/**
+	 * Make the path for the Chain.
+	 * 
+	 * @return {Error} If the path exists, it throws an error.
+	 */
 	__MakeChainPath__ () {
 
+		/**
+		 * Try create the path using the name of the Chain.
+		 * If it fails, except if the file exists, throw the Error.
+		 */
 		try {
 
 			FS.mkdirSync (Path.resolve (`./${this.ChainName}`));
@@ -83,8 +180,19 @@ class Chain {
 		}
 	}
 
+	/**
+	 * Make the Chain file saving in the path based on the Chain 
+	 * name in a ChainDB name file.
+	 * 
+	 * @return {Error}	Tell the Error that occurred.
+	 */
 	__MakeChainFile__ () {
 
+		/**
+		 * Try create the file of the chain inside of your Path based on your name,
+		 * puting inside it the Chain as a String.
+		 * If it fails, throw the Error.
+		 */
 		try {
 
 			FS.writeFileSync (`${this.ChainName}/chainDB`, JSON.stringify (this.Chain), "utf-8")
@@ -95,11 +203,24 @@ class Chain {
 		}
 	}
 
+	/**
+	 * Get the Chain file at your path based on your name and return it as an Array.
+	 * 
+	 * @param  {String} ChainName The name of the Chain.
+	 * @return {Error}	Tell the Error that occurred.
+	 */
 	__GetChainFile__ (ChainName) {
 
-
+		/**
+		 * Small helper variable.
+		 */
 		let Chain;
 
+
+		/**
+		 * Try read the Chain file at your path and assing it to the Chain variable.
+		 * If it fails, throw the Error.
+		 */
 		try {
 
 			Chain = FS.readFileSync (`./${this.ChainName}/chainDB`, {encoding: "utf-8"});
@@ -109,6 +230,14 @@ class Chain {
 			throw (SomeError);
 		}
 
+
+		/**
+		 * If the Chain variable exists, assign this Chain to the Chain variable 
+		 * after convert it to an Object.
+		 * 
+		 * @param  {Undefined/String} Chain The Chain file content.
+		 * @return {[type]}       [description]
+		 */
 		if (Chain) {
 
 			this.Chain = JSON.parse (Chain);
@@ -119,11 +248,27 @@ class Chain {
 	// 						Helpers 							//
 	//////////////////////////////////////////////////////////////
 
+
+	/**
+	 * Get the hash of the previous Block in the Chain.
+	 * 
+	 * @return {String} The Hash of the previous Block.
+	 */
 	get __PreviousBlockHash__ () {
 
+		/**
+		 * If the Chain has Blocks, return the hash of the last Block in the Chain.
+		 * If the Chain has no Blocks, return the PreviousBlockHash.
+		 */
 		return (this.Chain.length > 0 ? 
 			this.Chain [this.Chain.length - 1].CurrentBlockHash : GENESIS_BLOCK_HASH);
 	}
 }
 
+
+
+/**
+ * Export the Chain Class.
+ * @type {Object}
+ */
 module.exports = Chain;
