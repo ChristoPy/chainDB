bc6576f1ea82b969bb93e50d42570baa55b3df00
Christopher <christo_campiglia@hotmail.com>
2018-04-07 23:07:55 -0300

    [UPDATE] Tests based on the new changes of the Block.

--- Tests/index.js
+++ Tests/index.js
@@ -2,11 +2,11 @@ const Chain = require ("./../Chain.js");
 
 const MyChain0 = new Chain ("Test");
 
-MyChain0.NewBlock ("lorem ipsum");
-MyChain0.NewBlock (["lorem ipsum"]);
-MyChain0.NewBlock (0);
-MyChain0.NewBlock ([0, 1, 2, 3.1]);
-MyChain0.NewBlock ({
+MyChain0.NewBlock ("Title", "lorem ipsum");
+MyChain0.NewBlock ("Title inside an Array ", ["lorem ipsum"]);
+MyChain0.NewBlock ("Zero", 0);
+MyChain0.NewBlock ("Zero to 3.1 inside an Array", [0, 1, 2, 3.1]);
+MyChain0.NewBlock ("Object with all data before this Block", {
 	0: "lorem ipsum",
 	1: ["lorem ipsum"],
 	2: 0,
@@ -17,4 +17,4 @@ MyChain0.NewBlock ({
 
 const MyChain0Copy = new Chain ("Test");
 
-MyChain0Copy.NewBlock ("some new data");
+MyChain0Copy.NewBlock ("Update", "some new data");
