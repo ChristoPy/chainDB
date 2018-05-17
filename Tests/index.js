6a5b5c6a166f98793f92c52acbd2ab15ccf98733
Christopher <christo_campiglia@hotmail.com>
2018-05-17 19:44:13 -0300

    [UPDATE] Tests Code And Docs.

--- Tests/index.js
+++ Tests/index.js
@@ -1,12 +1,24 @@
+/**
+ * Get chainDB.
+ * @type {Object}
+ */
 const Chain = require ("./../Chain.js");
 
+
+/**
+ * Create an empty Chain.
+ * @type {Chain}
+ */
 const MyChain0 = new Chain ("Test");
 
-MyChain0.NewBlock ("Title", "lorem ipsum");
-MyChain0.NewBlock ("Title inside an Array", ["lorem ipsum"]);
+/**
+ * Add Blocks to the created Chain.
+ */
+MyChain0.NewBlock ("Standard Title", "lorem ipsum");
+MyChain0.NewBlock ("Standard Title inside an Array", ["lorem ipsum"]);
 MyChain0.NewBlock ("Zero", 0);
 MyChain0.NewBlock ("Zero to 3.1 inside an Array", [0, 1, 2, 3.1]);
-MyChain0.NewBlock ("Object with all data before this Block", {
+MyChain0.NewBlock ("Object with the same data as before", {
 	0: "lorem ipsum",
 	1: ["lorem ipsum"],
 	2: 0,
@@ -14,9 +26,26 @@ MyChain0.NewBlock ("Object with all data before this Block", {
 });
 
 
+/**
+ * Create a Chain with the same Blocks as MyChain0.
+ * @type {Chain}
+ */
+const MyChain1 = new Chain ("Test");
 
-const MyChain0Copy = new Chain ("Test");
+/**
+ * Add a Block to the Chain "Test".
+ */
+MyChain1.NewBlock ("I'm New Here!", "some new data");
 
-MyChain0Copy.NewBlock ("Update", "some new data");
 
-console.log (MyChain0Copy.GetBlock ("Update"));
+/**
+ * Should return the Block on the Chain.
+ * TO DO:
+ * 	Returns null, the Block is on the Chain but not referenced on MyChain0.
+ */
+// console.log (MyChain0.GetBlock ("I'm New Here!"));
+
+/**
+ * Return the Block on the Chain.
+ */
+console.log (MyChain1.GetBlock ("I'm New Here!"));
