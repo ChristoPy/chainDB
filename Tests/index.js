b55b6f1804cdc49109493dbf7a5bbe394dda8081
Christopher Ribeiro <christo_campiglia@hotmail.com>
2018-09-18 03:44:01 -0300

    [UPDATE] Tests.

--- Tests/index.js
+++ Tests/index.js
@@ -2,54 +2,54 @@
  * Get chainDB.
  * @type {Object}
  */
-const Chain = require ("./../index.js");
+const DB = require ("./../Distribution/Index.js");
 
 /**
  * Create a Chain.
  */
-Chain.New ("Example");
+DB.New ("Test");
 
 /**
  * Add two Blocks inside it.
  */
-Chain.AddBlock ("Standard Title", "lorem ipsum");
-Chain.AddBlock ("Standard Title inside an Array", ["lorem ipsum"]);
+DB.AddBlock ("Standard Title", "lorem ipsum");
+DB.AddBlock ("Standard Title inside an Array", ["lorem ipsum"]);
 
 /**
  * Add two another blocks with the same Name as before.
  */
-Chain.AddBlock ("Standard Title", "123");
-Chain.AddBlock ("Standard Title inside an Array", ["123"]);
+DB.AddBlock ("Standard Title", "123");
+DB.AddBlock ("Standard Title inside an Array", ["123"]);
 
 
 /**
  * Get the first Block with the name as "Standard Title inside an Array";
  */
-console.log (Chain.GetFirstBlock ("Standard Title inside an Array"));
+console.log (DB.GetFirstBlock ("Standard Title inside an Array"));
 
 /**
  * Get the last Block with the name as "Standard Title inside an Array";
  */
-console.log (Chain.GetLastBlock ("Standard Title inside an Array"));
+console.log (DB.GetLastBlock ("Standard Title inside an Array"));
 
 /**
  * Get all Blocks with the name as "Standard Title inside an Array";
  */
-console.log (Chain.GetAllBlocks ("Standard Title inside an Array"));
+console.log (DB.GetAllBlocks ("Standard Title inside an Array"));
 
 
 
 /**
  * Get the first Block with the name as "Standard Title";
  */
-console.log (Chain.GetFirstBlock ("Standard Title"));
+console.log (DB.GetFirstBlock ("Standard Title"));
 
 /**
  * Get the last Block with the name as "Standard Title";
  */
-console.log (Chain.GetLastBlock ("Standard Title"));
+console.log (DB.GetLastBlock ("Standard Title"));
 
 /**
  * Get all Blocks with the name as "Standard Title";
  */
-console.log (Chain.GetAllBlocks ("Standard Title"));
+console.log (DB.GetAllBlocks ("Standard Title"));
